0a350a2924528a0df92dc9a0f471fc9b44115787
BurningKarl <karl.welzel@gmail.com>
2019-06-27 20:42:14 +0200

    Added OneTree and minimumOneTree

--- AlphaDistances.cpp
+++ AlphaDistances.cpp
@@ -13,9 +13,7 @@
 #include "AlphaDistances.h"
 #include "PrimsAlgorithm.h"
 
-
-std::vector<std::vector<distance_t>>
-betaValues(dimension_t dimension, const std::function<distance_t(vertex_t, vertex_t)> &dist) {
+OneTree minimumOneTree(dimension_t dimension, const std::function<distance_t(vertex_t, vertex_t)> &dist) {
     std::vector<vertex_t> allVertices(dimension);
     std::iota(allVertices.begin(), allVertices.end(), 0);
 
@@ -24,8 +22,6 @@ betaValues(dimension_t dimension, const std::function<distance_t(vertex_t, verte
 
     // Generate a minimum spanning tree in the complete graph
     std::tie(parent, topologicalOrder) = primsAlgorithm(dimension, dist);
-
-    // TODO: Use subgradient optimization
 
     // Determine the special vertex "1" by selecting the leaf with longest second nearest neighbor distance
     std::unordered_set<vertex_t> leafs(allVertices.begin(), allVertices.end());
@@ -46,34 +42,40 @@ betaValues(dimension_t dimension, const std::function<distance_t(vertex_t, verte
                                              return dist(v, secondNearestNeighbor(v)) <
                                                     dist(w, secondNearestNeighbor(w));
                                          });
-    vertex_t specialNeighbor = secondNearestNeighbor(special);
+
+    // remove special from topologicalOrder
     topologicalOrder.erase(std::remove(topologicalOrder.begin(), topologicalOrder.end(), special),
                            topologicalOrder.end());
 
-    // The edge (special, specialNeighbor) is added to the spanning tree to make a 1-tree, so that the two neighbors
-    // of special are parent[special] and specialNeighbor with
-    //     dist(special, parent[special]) < dist(special, specialNeighbor)
+    // Determine the second nearest neighbor of special
+    vertex_t specialNeighbor = secondNearestNeighbor(special);
 
+    return OneTree{parent, topologicalOrder, special, specialNeighbor};
+}
+
+
+std::vector<std::vector<distance_t>>
+betaValues(OneTree tree, dimension_t dimension, const std::function<distance_t(vertex_t, vertex_t)> &dist) {
     // Initialize the beta array
     std::vector<std::vector<distance_t>> beta(dimension, std::vector<distance_t>(dimension, 0));
 
     // Set the beta values for edges (special, v)
-    for (vertex_t vertex : topologicalOrder) {
-        if (vertex == parent[special]) {
+    for (vertex_t vertex : tree.topologicalOrder) {
+        if (vertex == tree.parent[tree.special]) {
             // alpha(special, vertex) = 0 so beta(special, vertex) = dist(special, vertex)
-            beta[special][vertex] = beta[vertex][special] = dist(special, vertex);
+            beta[tree.special][vertex] = beta[vertex][tree.special] = dist(tree.special, vertex);
         } else {
             // When (special, vertex) is required to be in the 1-tree then the edge incident with special with highest
             // distance needs to be removed, namely (special, specialNeighbor)
-            beta[special][vertex] = beta[vertex][special] = dist(special, specialNeighbor);
+            beta[tree.special][vertex] = beta[vertex][tree.special] = dist(tree.special, tree.specialNeighbor);
         }
     }
 
     // Compute the beta values for all other edges as described in the paper by Keld Helsgaun from 2000
-    for (auto i = topologicalOrder.begin(); i != topologicalOrder.end(); ++i) {
-        for (auto j = i + 1; j != topologicalOrder.end(); ++j) {
+    for (auto i = tree.topologicalOrder.begin(); i != tree.topologicalOrder.end(); ++i) {
+        for (auto j = i + 1; j != tree.topologicalOrder.end(); ++j) {
             // beta[x][x] has the smallest possible value 0, so it will never be chosen here
-            beta[*i][*j] = beta[*j][*i] = std::max(beta[*i][parent[*j]], dist(*j, parent[*j]));
+            beta[*i][*j] = beta[*j][*i] = std::max(beta[*i][tree.parent[*j]], dist(*j, tree.parent[*j]));
         }
     }
 
@@ -82,13 +84,18 @@ betaValues(dimension_t dimension, const std::function<distance_t(vertex_t, verte
 
 std::vector<std::vector<distance_t>>
 alphaDistances(dimension_t dimension, const std::function<distance_t(vertex_t, vertex_t)> &dist) {
-    std::vector<std::vector<distance_t>> beta = betaValues(dimension, dist);
+    // Compute the beta values
+    std::vector<std::vector<distance_t>> beta = betaValues(minimumOneTree(dimension, dist), dimension, dist);
+
+    // Initialize the alpha array
     std::vector<std::vector<distance_t>> alpha(dimension, std::vector<distance_t>(dimension, 0));
 
+    // Compute the alpha values
     for (size_t i = 0; i < dimension; ++i) {
         for (size_t j = 0; j < dimension; ++j) {
             alpha[i][j] = dist(i, j) - beta[i][j];
         }
     }
+
     return alpha;
 }
--- AlphaDistances.h
+++ AlphaDistances.h
@@ -10,6 +10,27 @@
 #include <vector>
 #include "Tour.h"
 
+// This struct represents a 1-tree
+struct OneTree {
+    // parent maps each vertex to its parent in the ordinary minimum spanning tree
+    std::vector<vertex_t> parent;
+
+    // topologicalOrder contains all vertices except special in an order such that parent[v] comes before v for every
+    // vertex v
+    std::vector<vertex_t> topologicalOrder;
+
+    // The special "1" vertex
+    vertex_t special;
+
+    // (special, specialNeighbor) is the additional edge added to the tree to form a 1-tree
+    vertex_t specialNeighbor;
+};
+
+// Returns a minimum 1-tree by computing a minimum spanning tree in the complete graph, choosing the leaf with the
+// longest second nearest neighbor distance as the special vertex and adding the edge to special's second nearest
+// neighbor to the tree. special is incident to the edges (special, parent[special]) and (special, specialNeighbor)
+// It is guaranteed that dist(special, parent[special]) <= dist(special, specialNeighbor)
+OneTree minimumOneTree(OneTree tree, dimension_t dimension, const std::function<distance_t(vertex_t, vertex_t)> &dist);
 
 std::vector<std::vector<distance_t>>
 betaValues(dimension_t dimension, const std::function<distance_t(vertex_t, vertex_t)> &dist);
